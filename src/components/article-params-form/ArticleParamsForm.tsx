874f82c1ccde30cf3d9742f9a8ad988b06bbf400
DashaShoo <daryashubabko@gmail.com>
2025-04-15 17:58:14 +0300

    боковая панель открывается

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,24 +1,25 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [open, setOpen] = useState(false);
+
+
 	return (
 		<>
-			<ArrowButton />
-			<aside
-				className={styles.container}>
+			<ArrowButton open={open} setOpen={setOpen} />
+			<aside className={clsx(styles.container, open && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
-						<Button
-							title='Сбросить'
-							type='reset'
-						/>
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
-			</aside>
+					<Button title='Сбросить' type='reset' />
+ 						<Button title='Применить' type='submit' />
+ 					</div>
+ 				</form>
+ 			</aside>
 		</>
 	);
 };
